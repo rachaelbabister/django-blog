747d7b0368c5c3d8011cf3bbfe5c1e5657e83569
rachaelbabister <rachaelbabister@gmail.com>
2024-04-11 15:04:20 +0000

    Modify admin dashboard and integrate Summernote package

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-rachaelbabister-django-b-19mjeobzga.us1.codeanyapp.com', '.herokuapp.com']
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'blog',
 ]
 
