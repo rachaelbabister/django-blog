b1dcdf72a7d458dd0ca9a639237da66f670ab04b
rachaelbabister <rachaelbabister@gmail.com>
2024-04-15 14:42:58 +0000

    Change debug to False in settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-rachaelbabister-django-b-19mjeobzga.us1.codeanyapp.com', '.herokuapp.com']
 
